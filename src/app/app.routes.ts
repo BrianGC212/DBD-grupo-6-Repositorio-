8d249f0f9d3baaba013535f6e9fdc6ca5b3824bd
Aaron-IMP <aaron.machaca.p@uni.pe>
2025-06-23 01:36:27 -0400

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,16 @@ import { VisualizarLotes } from './components/recepcion/visualizar-lotes/visuali
 import { DashboardRecepcion } from './components/recepcion/dashboard-recepcion/dashboard-recepcion';
 import { ControlCalidad } from './components/recepcion/control-calidad/control-calidad';
 import { GeneracionReportes } from './components/recepcion/generacion-reportes/generacion-reportes';
+import { VisualizarOrdenesTransporte } from './components/gestion-transporte/visualizar-ordenes-transporte/visualizar-ordenes-transporte';
+import { InicioGestionTransporte } from './components/gestion-transporte/inicio-gestion-transporte/inicio-gestion-transporte';
+import { VisualizarGuiasRemision } from './components/gestion-transporte/visualizar-guias-remision/visualizar-guias-remision';
+import { VisualizarInformeEntrega } from './components/gestion-transporte/visualizar-informe-entrega/visualizar-informe-entrega';
+import { VisualizarReporteIncidente } from './components/gestion-transporte/visualizar-reporte-incidente/visualizar-reporte-incidente';
+import { RegistrarGuiaRemision } from './components/gestion-transporte/registrar-guia-remision/registrar-guia-remision';
+import { Seguimiento } from './components/gestion-transporte/seguimiento/seguimiento';
+import { RegistrarSeguimiento } from './components/gestion-transporte/registrar-seguimiento/registrar-seguimiento';
+import { RegistrarInformeEntrega } from './components/gestion-transporte/registrar-informe-entrega/registrar-informe-entrega';
+import { RegistrarOrdenTransporte } from './components/gestion-transporte/registrar-orden-transporte/registrar-orden-transporte';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'sidebar', pathMatch: 'full' },
@@ -18,5 +28,15 @@ export const routes: Routes = [
     { path: 'visualizar-lotes', component: VisualizarLotes },
     { path: 'control-calidad', component: ControlCalidad },
     { path: 'reportes-recepcion', component: GeneracionReportes },
+    {path: 'gestion-transporte', component:InicioGestionTransporte},
+    {path: 'gestion-transporte/ordenes', component:VisualizarOrdenesTransporte},
+    { path: 'gestion-transporte/guias', component: VisualizarGuiasRemision },
+    { path: 'gestion-transporte/informes', component: VisualizarInformeEntrega },
+    { path: 'gestion-transporte/incidentes', component: VisualizarReporteIncidente },
+    {path: 'gestion-transporte/registrar-guia', component: RegistrarGuiaRemision},
+    {path: 'gestion-transporte/seguimiento', component: Seguimiento},
+    {path: 'gestion-transporte/registrar-seguimiento',component: RegistrarSeguimiento},
+    { path: 'gestion-transporte/registrar-informe-entrega', component: RegistrarInformeEntrega },
+    { path: 'gestion-transporte/registrar-orden', component: RegistrarOrdenTransporte },
     { path: '**', redirectTo: 'sidebar' }
 ];
