64bbc036a62f35c6b397425e776b6bc942da5ca2
BrianGC212 <brian.gonzales.c@uni.pe>
2025-06-28 23:20:14 -0500

    Add files via upload

---
+++ logistica_agrovision - copia/log-agrov/src/main/java/logistica_agrovision/log_agrov/Models/Equipo.java
@@ -0,0 +1,13 @@
+package logistica_agrovision.log_agrov.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Equipo {
+    private int idEquipo;
+    private String nombre;
+}
